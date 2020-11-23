6761265d9df52446f6e42be634d376867a6a3ec2
unknown <miguelborquez7@gmail.com>
2020-11-23 13:48:26 -0800

    clear once

---
+++ app/Service.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    // Add Function
+    public function name(){
+        return "Services";
+    }
+}
