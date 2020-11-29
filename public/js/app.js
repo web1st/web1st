88f96a2ff65ba68e3f5472ae7eb423e41c591cc0
unknown <miguelborquez7@gmail.com>
2020-11-28 16:06:35 -0800

    working menue

--- public/js/app.js
+++ public/js/app.js
@@ -19366,26 +19366,26 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/main.scss":
-/*!**********************************!*\
-  !*** ./resources/sass/main.scss ***!
-  \**********************************/
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleNotFoundError: Module not found: Error: Can't resolve './fontawesome-all.min.css' in 'C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\resources\\sass'\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\webpack\\lib\\Compilation.js:925:10\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\webpack\\lib\\NormalModuleFactory.js:401:22\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\webpack\\lib\\NormalModuleFactory.js:130:21\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\webpack\\lib\\NormalModuleFactory.js:224:22\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\neo-async\\async.js:2830:7\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\neo-async\\async.js:6877:13\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\webpack\\lib\\NormalModuleFactory.js:214:25\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\Resolver.js:213:14\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\UnsafeCachePlugin.js:44:7\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:27:1)\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:67:43\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:16:1)\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:27:1)\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\DescriptionFilePlugin.js:67:43\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:16:1)\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\Resolver.js:285:5\n    at eval (eval at create (C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\DirectoryExistsPlugin.js:27:15\n    at C:\\xampp\\htdocs\\lrvl_projects\\web1st_solutions\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:75:11)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
 /***/ 0:
-/*!**************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/main.scss ***!
-  \**************************************************************/
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\xampp\htdocs\lrvl_projects\web1st_solutions\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\lrvl_projects\web1st_solutions\resources\sass\main.scss */"./resources/sass/main.scss");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\lrvl_projects\web1st_solutions\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
